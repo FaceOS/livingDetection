dd83599ce031be9f109d8133d2eeec206dc14468
pengjiaxin <pengjiaxin@faceos.com>
2021-05-14 11:47:01 +0800

    add BitmapZoomUtils

--- app/src/main/java/com/renren/faceos/fragment/IdentityFragment.java
+++ app/src/main/java/com/renren/faceos/fragment/IdentityFragment.java
@@ -43,8 +43,8 @@ public class IdentityFragment extends BaseFragment implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
-        name.setText("张大利");
-        idCard.setText("152221198906101419");
+//        name.setText("张大利");
+//        idCard.setText("152221198906101419");
         String nameStr = name.getText().toString();
         String idCardStr = idCard.getText().toString();
         if (TextUtils.isEmpty(nameStr)) {

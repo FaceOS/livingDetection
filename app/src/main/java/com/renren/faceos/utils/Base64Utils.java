1159a4d0906f5b78f46e5d11c4834d39379afcc1
pengjiaxin <pengjiaxin@faceos.com>
2021-05-21 10:54:25 +0800

    update compress 100k

--- app/src/main/java/com/renren/faceos/utils/Base64Utils.java
+++ app/src/main/java/com/renren/faceos/utils/Base64Utils.java
@@ -3,12 +3,12 @@ package com.renren.faceos.utils;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.util.Base64;
+import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class Base64Utils {
-
 
 
     /*
@@ -17,16 +17,24 @@ public class Base64Utils {
     public static String bitmapToBase64(Bitmap bitmap) {
         String result = null;
         ByteArrayOutputStream baos = null;
+        int quality = 100;
         try {
             if (bitmap != null) {
                 baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-
+                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
                 baos.flush();
-                baos.close();
-
                 byte[] bitmapBytes = baos.toByteArray();
                 result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+                //压缩base64到指定大小
+                while (result.length() / 1024 > 100) {
+                    quality -= 1;
+                    baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+                    baos.flush();
+                    bitmapBytes = baos.toByteArray();
+                    result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+                }
+                Log.e("eeee", "1111111114 " + result.length() / 1024);
             }
         } catch (IOException e) {
             e.printStackTrace();
--- app/src/main/java/com/renren/faceos/utils/BitmapZoomUtils.java
+++ app/src/main/java/com/renren/faceos/utils/BitmapZoomUtils.java
@@ -69,8 +69,8 @@ public class BitmapZoomUtils {
         // 现在主流手机比较多是800*480分辨率，所以高和宽我们设置为
         // float hh = 800f;// 这里设置高度为800f
         // float ww = 480f;// 这里设置宽度为480f
-        float hh = 300f;
-        float ww = 300f;
+        float hh = 500f;
+        float ww = 500f;
         // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
         int be = 1;// be=1表示不缩放
         if (w > h && w > ww) {// 如果宽度大的话根据宽度固定大小缩放
@@ -99,11 +99,11 @@ public class BitmapZoomUtils {
     public static Bitmap compressImage(Bitmap image) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         image.compress(Bitmap.CompressFormat.JPEG, 100, baos);// 质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
-        int options = 90;
+        int options = 99;
         while (baos.toByteArray().length / 1024 > 100) { // 循环判断如果压缩后图片是否大于100kb,大于继续压缩
             baos.reset(); // 重置baos即清空baos
             image.compress(Bitmap.CompressFormat.JPEG, options, baos);// 这里压缩options%，把压缩后的数据存放到baos中
-            options -= 10;// 每次都减少10
+            options -= 1;// 每次都减少10
         }
         ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());// 把压缩后的数据baos存放到ByteArrayInputStream中
         Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
